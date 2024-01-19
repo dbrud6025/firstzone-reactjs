280e97ac1e69b67f7abf71776aaa156aa2fe15e0
Yang Yukyung <mapu6025@gmail.com>
2024-01-19 12:07:46 +0900

    01/19 점심 전까지 수업내용

--- src/component3/LifeCycleClassComponent.js
+++ src/component3/LifeCycleClassComponent.js
@@ -24,9 +24,64 @@ class LifeCycleClassComponent extends Component {
   }
 
   render() {
+    console.log("LifeCycleClassComponent...render");
+    const { title, buttonVisible } = this.state;
     return (
       <div>
-        <h1>LifeCycleClassComponent 연습!!!</h1>
+        <h1>LifeCycleClassComponent 연습!!! : {title}</h1>
+        <button
+          onClick={() =>
+            this.setState({ title: "보이기!", buttonVisible: true })
+          }
+        >
+          자식 컴포넌트 보이기~~
+        </button>
+        {buttonVisible && <div></div>}
+      </div>
+    );
+  }
+}
+
+class LifeCycleClassComponent2 extends Component {
+  constructor(props) {
+    super(props);
+    console.log("LifeCycleClassComponent........constructor");
+  }
+
+  componentDidMount() {
+    console.log("LifeCycleClassComponent........componentDidMount (생성됨)");
+  }
+
+  shouldComponentUpdate(nextProps, nextState) {
+    console.log("LifeCycleClassComponent........shouldComponentUpdate");
+  }
+
+  componentDidUpdate(prevProps, prevState) {
+    console.log("LifeCycleClassComponent........componentDidUpdate (수정됨)");
+  }
+
+  componentWillUnmount() {
+    console.log("LifeCycleClassComponent........componentWillUnmount (제거)");
+  }
+
+  render() {
+    console.log("LifeCycleClassComponent...render");
+    const { title, buttonVisible } = this.state;
+    return (
+      <div>
+        <h1>LifeCycleClassComponent 연습!!! : {title}</h1>
+        <button
+          onClick={() =>
+            this.setState({ title: "보이기!", buttonVisible: true })
+          }
+        >
+          자식 컴포넌트 보이기~~
+        </button>
+        {buttonVisible && (
+          <div>
+            <LifeCycleClassComponent2></LifeCycleClassComponent2>
+          </div>
+        )}
       </div>
     );
   }
